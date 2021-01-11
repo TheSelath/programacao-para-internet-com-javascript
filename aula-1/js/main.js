e31200dc332c9b015625574cc0e4e27b463dcf88
Thales Domingos <thales.domingos93@gmail.com>
2021-01-11 17:56:12 -0300

    feita aula 1

---
+++ aula-1/js/main.js
@@ -0,0 +1,23 @@
+var nome = "Thales Domingos";
+var idade = 27;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+var n1 = 5;
+var n2 = 3;
+
+// alert(nome +"tem" + idade + "anos");
+// alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+console.log(n1 * n2);
+
+//alert(frase.replace("Japão", "Brasil"));
+
+
+
