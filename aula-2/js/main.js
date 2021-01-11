cacc50e12696b1745b38e50981a803a3d5618f98
Thales Domingos <thales.domingos93@gmail.com>
2021-01-11 18:33:36 -0300

    aula 3

---
+++ aula-2/js/main.js
@@ -0,0 +1,32 @@
+var lista = ["maça", "pêra", "laranja"]; //array
+console.log(lista);
+console.log(lista[1]);
+
+lista.push("uva");/* acrescenta elemento no array*/
+
+console.log(lista);
+
+//lista.pop(); remove ultimo elemento do array
+
+//alert(lista[1]);
+
+console.log(lista.length);
+
+console.log(lista.reverse());
+
+console.log(lista.toString()); /* converte elementos em string*/
+
+console.log(lista.join(" - " )); /* troca o tipo de separador e transforma em string */
+
+var fruta = {nome: "maça", cor: "vermelha"}; //dicionario
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+
+// alert(fruta.cor);
+
+var frutas = [{nome:"maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+
+console.log(frutas);
+
+// alert(frutas[1].nome);
---
+++ aula-3/js/main.js
@@ -0,0 +1,42 @@
+
+var d =  new Date();
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getHours());
+
+
+/*
+var count;
+
+for (count = 0; count <= 5; count++){
+    console.log(count);
+};
+
+*/
+
+/*
+
+var count = 5;
+while(count < 5){
+console.log(count);
+count = count +1; //ou caunt++
+
+};
+
+*/
+
+/*
+
+var idade = prompt("qual sua idade?") //exibe pergunta e grava na variavel
+
+// var idade = 18;
+
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade!");
+};
+
+*/
